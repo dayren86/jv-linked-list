975b0bafd00f9a65f3b4b93026b9949e28a15750
unknown <dayren86@gmail.com>
2025-10-30 22:18:11 +0200

    linkedlist v1

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -3,12 +3,43 @@ package core.basesyntax;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+    private Node<T> headNode;
+    private Node<T> tailNode;
+//    private int size = 0;
+
     @Override
     public void add(T value) {
+        if (headNode == null) {
+            Node<T> tNode = new Node<>(null, value, null);
+            headNode = tNode;
+            tailNode = tNode;
+        } else {
+            Node<T> tNode = new Node<>(tailNode, value, null);
+            tailNode.nextNode = tNode;
+            tailNode = tNode;
+        }
     }
 
     @Override
     public void add(T value, int index) {
+        if (headNode == null) {
+            Node<T> tNode = new Node<>(null, value, null);
+            headNode = tNode;
+            tailNode = tNode;
+        } else {
+            Node<T> nodeByIndex = findNodeByIndex(index);
+            if (nodeByIndex == null) {
+                add(value);
+            } else if (index == 0) {
+                Node<T> tNode = new Node<>(nodeByIndex.prevNode, value, nodeByIndex);
+                nodeByIndex.prevNode = tNode;
+                headNode = tNode;
+            }else {
+                Node<T> tNode = new Node<>(nodeByIndex.prevNode, value, nodeByIndex);
+                nodeByIndex.prevNode = tNode;
+
+            }
+        }
     }
 
     @Override
@@ -17,7 +48,7 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     @Override
     public T get(int index) {
-        return null;
+        return findNodeByIndex(index).value;
     }
 
     @Override
@@ -37,11 +68,42 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     @Override
     public int size() {
-        return 0;
+        int countSize = 0;
+        Node<T> findLastNode = headNode;
+        while (findLastNode.nextNode != null){
+            countSize++;
+        }
+        return countSize;
     }
 
     @Override
     public boolean isEmpty() {
         return false;
+    }
+
+    private Node<T> findNodeByIndex(int index) {
+//        if (index > size()) {
+//            throw new IndexOutOfBoundsException("index " + index);
+//        }
+
+        int startIndex = 0;
+        Node<T> findNode = headNode;
+        while (startIndex != index) {
+            findNode = findNode.nextNode;
+            startIndex++;
+        }
+        return findNode;
+    }
+
+    static class Node<T> {
+        private Node<T> prevNode;
+        private T value;
+        private Node<T> nextNode;
+
+        public Node(Node<T> prevNode, T value, Node<T> nextNode) {
+            this.prevNode = prevNode;
+            this.value = value;
+            this.nextNode = nextNode;
+        }
     }
 }
